54f40060cc9b146519695111b44acdc9042b719d
Elimar Lucena <elimarlucenar@gmail.com>
2022-04-22 12:29:06 -0300

    style: basic game structure

--- tic-tac-toe/src/Components/TicTacToe/index.js
+++ tic-tac-toe/src/Components/TicTacToe/index.js
@@ -4,18 +4,20 @@ import './style.css';
 function TicTacToe() {
   return (
     <main className="main-container">
-      <h1 className="game-title">TicTacToe</h1>
-      <section className="game-conatiner">
-          <button className="box 1">?</button>
-          <button className="box 2">?</button>
-          <button className="box 3">?</button>
-          <button className="box 4">?</button>
-          <button className="box 5">?</button>
-          <button className="box 6">?</button>
-          <button className="box 7">?</button>
-          <button className="box 8">?</button>
-          <button className="box 9">?</button>
-      </section>
+      <div className="game-container">
+        <h1 className="game-title">TicTacToe</h1>
+        <section className="box-conatiner">
+            <button className="box 1">?</button>
+            <button className="box 2">?</button>
+            <button className="box 3">?</button>
+            <button className="box 4">?</button>
+            <button className="box 5">?</button>
+            <button className="box 6">?</button>
+            <button className="box 7">?</button>
+            <button className="box 8">?</button>
+            <button className="box 9">?</button>
+        </section>
+      </div>
     </main>
   )
 }
