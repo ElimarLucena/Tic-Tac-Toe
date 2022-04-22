aa652dc110380109dd787c928e13f7010d30c469
Elimar Lucena <elimarlucenar@gmail.com>
2022-04-22 11:08:38 -0300

    feat: body of the game

--- tic-tac-toe/src/Components/TicTacToe/index.js
+++ tic-tac-toe/src/Components/TicTacToe/index.js
@@ -4,6 +4,23 @@ function TicTacToe() {
   return (
     <main>
       <h1>TicTacToe</h1>
+      <section>
+        <section>
+          <button className="box-1">?</button>
+          <button className="box-2">?</button>
+          <button className="box-3">?</button>
+        </section>
+        <section>
+          <button className="box-4">?</button>
+          <button className="box-5">?</button>
+          <button className="box-6">?</button>
+        </section>
+        <section>
+          <button className="box-7">?</button>
+          <button className="box-8">?</button>
+          <button className="box-9">?</button>
+        </section>
+      </section>
     </main>
   )
 }
