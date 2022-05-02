2c62a7a79a25032816be15f3a3bd5c24d5c78807
Elimar Lucena <elimarlucenar@gmail.com>
2022-05-02 07:37:11 -0300

    feat: files where game routes will be.

---
+++ tic-tac-toe/src/Routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+
+function Roads() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={ <Login /> } />
+      <Route path="" element={ </> } /> */}
+    </Routes> 
+  )
+}
+
+export default Roads;
