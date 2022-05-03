786ecefeb22ab73ea8428312942ffafb6ee52c7f
Elimar Lucena <elimarlucenar@gmail.com>
2022-05-03 20:40:14 -0300

    feat: button for login page

--- tic-tac-toe/src/Pages/Login/index.js
+++ tic-tac-toe/src/Pages/Login/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <section>
       <h1>Tic Tac Toe</h1>
+      <button
+        type="button"
+        onClick={ () =>  navigate('/tictactoe') }
+      >
+        Start
+      </button>
     </section>
   )
 }
