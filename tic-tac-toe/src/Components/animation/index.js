2477d149f1365859404f1f5a5e8d5d0005de181b
Elimar Lucena <elimarlucenar@gmail.com>
2022-05-03 22:16:11 -0300

    feat: animation component

---
+++ tic-tac-toe/src/Components/animation/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+// Source: https://www.youtube.com/watch?v=DykPXQUO-QY&ab_channel=N%C3%A1sserYousefAli
+
+function Animation() {
+  return (
+    <section className="container-animation">
+      <section className="animation">
+        <div className="ball ball-1">X</div>
+        <div className="ball ball-2">O</div>
+      </section>
+    </section>
+  )
+}
+
+export default Animation;
--- tic-tac-toe/src/Pages/Login/index.js
+++ tic-tac-toe/src/Pages/Login/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import Animation from '../../Components/animation';
 import './style.css'
 
 function Login() {
   const navigate = useNavigate();
   return (
     <section className="conatiner-login">
+      <Animation />
       <h1 className="title-login">Tic Tac Toe</h1>
       <button
         type="button"
