c94cb4524576fd93a81b333779f7033c93069fb9
Elimar Lucena <elimarlucenar@gmail.com>
2022-05-04 09:01:42 -0300

    style:redoing the style of login page

--- tic-tac-toe/src/Pages/Login/index.js
+++ tic-tac-toe/src/Pages/Login/index.js
@@ -10,7 +10,10 @@ function Login() {
       {/* <div className="animation-login">
         <Animation />
       </div> */}
-      <h1 className="title-login">Tic Tac Toe</h1>
+      <div className="conatiner-title">
+        <h1 className="title-login">Tic Tac Toe</h1>
+        <h2>😄Lets Go!🚀</h2>
+      </div>
       <button
         type="button"
         className="button-login"
