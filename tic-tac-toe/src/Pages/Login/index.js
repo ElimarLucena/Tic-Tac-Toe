fb361d4f899f203588111ee1352b7ed04be042d6
Elimar Lucena <elimarlucenar@gmail.com>
2022-05-03 22:48:18 -0300

    style: removing animation

--- tic-tac-toe/src/Pages/Login/index.js
+++ tic-tac-toe/src/Pages/Login/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import Animation from '../../Components/animation';
+// import Animation from '../../Components/animation';
 import './style.css'
 
 function Login() {
   const navigate = useNavigate();
   return (
     <section className="conatiner-login">
-      <div className="animation-login">
+      {/* <div className="animation-login">
         <Animation />
-      </div>
+      </div> */}
       <h1 className="title-login">Tic Tac Toe</h1>
       <button
         type="button"
