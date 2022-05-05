befbc18cbe9c8bd7401ba2943776c606d4dc5a3d
Elimar Lucena <elimarlucenar@gmail.com>
2022-05-05 09:06:10 -0300

    style: removing unnecessary comments

--- tic-tac-toe/src/Pages/Login/index.js
+++ tic-tac-toe/src/Pages/Login/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-// import Animation from '../../Components/animation';
 import './style.css';
 
 function Login() {
   const navigate = useNavigate();
   return (
     <section className="conatiner-login">
-      {/* <div className="animation-login">
-        <Animation />
-      </div> */}
       <div className="conatiner-title">
         <h1 className="title-login">Tic Tac Toe</h1>
         <h2>😄Lets Go!🚀</h2>
